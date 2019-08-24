0c80566e93c9d52a2e8cf0b237dc05c678eec898
Graham Flaspoehler <grahamflas@gmail.com>
2019-08-24 00:42:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def new_hash
     new_hash = {}
 end
-
+  
 def my_hash
     my_hash = {
         :name => "Graham",
@@ -9,32 +9,39 @@ def my_hash
         :l2 => "Deutsch"
     }
 end
-
+  
 def pioneer
     pioneer_hash = {
         name: "Grace Hopper"
     }
 end
-
+  
 def id_generator
     id_hash = {
         id: 12
     }
 end
-
+  
 def my_hash_creator(key, value)
     my_created_hash = {
         key => value
     }
 end
-
+  
 def read_from_hash(hash, key)
     hash[key]
 end
-
+  
 def update_counting_hash(hash, key)
     # given a hash an a key as parameters, return an updated hash
     # if the provided key is not present in the hash, add it and assign it to the value of 1
     # if the provided key is present, increment its value by 1
+    if hash[key]
+        hash[key] += 1
+    end
+    else
+        hash[key] = 1
+    end
+
 end
   
