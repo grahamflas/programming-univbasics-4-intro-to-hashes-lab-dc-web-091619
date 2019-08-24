3ebc11d7a6809fa56788d9268eaa4a0c1cc5ad89
Graham Flaspoehler <grahamflas@gmail.com>
2019-08-24 00:31:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def new_hash
 end
 
 def my_hash
-    # return a valid hash with any key/value pair of your choice
+    my_hash = {
+        :name = "Graham"
+        :l1 = "English"
+        :l2 = "Deutsch"
+    }
 end
 
 def pioneer
@@ -21,10 +25,9 @@ end
 def read_from_hash(hash, key)
     # return the correct value using the hash and key parameters
 end
-
+  
 def update_counting_hash(hash, key)
     # given a hash an a key as parameters, return an updated hash
     # if the provided key is not present in the hash, add it and assign it to the value of 1
     # if the provided key is present, increment its value by 1
 end
-  
